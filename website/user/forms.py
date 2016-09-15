b0cd933749d29c8a9370e9048bab762fcfa51f14
Dorian Amouroux <dor.amouroux@gmail.com>
2016-09-15 23:39:56 +0200

    email uniqueness in validator

--- website/user/forms.py
+++ website/user/forms.py
@@ -1,10 +1,14 @@
 from django import forms
 from django.contrib.auth.models import User
 
-from .validators import UsernameValidator
+from .validators import UsernameValidator, emailUniqueValidator
 
 class RegisterForm(forms.ModelForm):
-    email = forms.CharField(max_length=75, required=True)
+    email = forms.CharField(
+        max_length=75,
+        validators=[emailUniqueValidator],
+        required=True
+    )
     username = forms.CharField(
         max_length=75,
         validators=[UsernameValidator()],
@@ -15,10 +19,3 @@ class RegisterForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['email', 'username', 'password']
-
-    def clean_email(self):
-        email = self.cleaned_data.get('email')
-        username = self.cleaned_data.get('username')
-        if email and User.objects.filter(email=email).exclude(username=username).count():
-            raise forms.ValidationError(u'A user with that email address already exists.')
-        return email
