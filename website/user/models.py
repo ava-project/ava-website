06293f84db4ed85eeee02732dc769c67a4f97839
Dorian Amouroux <dor.amouroux@gmail.com>
2016-09-16 23:18:58 +0200

    create profile extension on user create

--- website/user/models.py
+++ website/user/models.py
@@ -1,7 +1,16 @@
 from django.db import models
+from django.db.models.signals import post_save
 from django.contrib.auth.models import User
+from django.dispatch import receiver
 
-# Create your models here.
 
-class profile(models.Model):
+class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+"""
+Signal to create a profile model when a User is created
+"""
+@receiver(post_save, sender=User)
+def after_user_save(sender, **kwargs):
+    if kwargs['created']:
+        Profile(user=kwargs['instance']).save()
