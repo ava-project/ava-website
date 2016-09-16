376364883ee06ad443a2eddfbe77d5772e8f443e
Dorian Amouroux <dor.amouroux@gmail.com>
2016-09-16 21:04:07 +0200

    test account creation

--- website/user/tests.py
+++ website/user/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+import unittest
+from django.test import Client
 
-# Create your tests here.
+class DuplicateTest(unittest.TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_register(self):
+        # Issue a GET request.
+        response = self.client.post('/user/register/', {
+            'username': 'test',
+            'email': 'test@test.fr',
+            'password': 'test',
+        })
+        self.assertEqual(response.status_code, 302)
