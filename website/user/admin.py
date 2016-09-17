cd7814a7c04791806f3c431cab064862166b364a
Dorian Amouroux <dor.amouroux@gmail.com>
2016-09-17 22:30:58 +0200

    better django admin for user

--- website/user/admin.py
+++ website/user/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.utils.translation import ugettext, ugettext_lazy as _
 
-from .models import EmailValidationToken
+from .models import EmailValidationToken, Profile
 
 class EmailValidationTokenAdmin(admin.ModelAdmin):
     list_display = ('user', 'token', 'consumed')
@@ -8,3 +10,23 @@ class EmailValidationTokenAdmin(admin.ModelAdmin):
     readonly_fields = ('user', 'token', 'consumed',)
 
 admin.site.register(EmailValidationToken, EmailValidationTokenAdmin)
+
+class ProfileInline(admin.StackedInline):
+    """ Details a person in line. """
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'profile'
+
+
+class MyUserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'first_name', 'last_name',)
+    list_filter = ('is_staff', 'is_superuser')
+    fieldsets = (
+        (_('Personal info'), {'fields': ('username' ,'first_name', 'last_name', 'email')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+        (_('Important dates'), {'fields': ('date_joined', 'last_login',)}),
+    )
+    inlines = [ProfileInline]
+
+admin.site.unregister(User)
+admin.site.register(User, MyUserAdmin)
