8cb12fe6384fb2c7ad65f9cbfe61bb91d10f6bfc
Dorian Amouroux <dor.amouroux@gmail.com>
2016-10-01 14:29:45 +0200

    put template in base dir send mail

--- website/core/settings/common/template.py
+++ website/core/settings/common/template.py
@@ -1,7 +1,13 @@
+import os
+
+from . import BASE_DIR
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, "templates"),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- website/user/views.py
+++ website/user/views.py
@@ -1,16 +1,31 @@
 from django.contrib import auth
 from django.contrib.auth.models import User
+from django.http import HttpResponseBadRequest
 from django.shortcuts import render, redirect
 from django.views.generic.edit import FormView
 from django.views.generic import TemplateView
 
+from core.utils import send_email
+
 from . import forms
 from .models import EmailValidationToken
 
-
+"""
+This endpoint is a generic form view for the user
+registration. The validation of the form is done
+by the validators on the model layer. Then it
+creates the user, the validation token and it
+sends the email, then redirect to the homepage
+"""
 class RegisterView(FormView):
     template_name = "user/register.html"
     form_class = forms.RegisterForm
+
+    def send_validation_email(self, user, token):
+        send_email(
+            'user/email_register.html',
+            user.email,
+            token=token)
 
     def form_valid(self, form):
         data = form.cleaned_data
@@ -19,7 +34,9 @@ class RegisterView(FormView):
             data['email'],
             data['password']
         )
-        EmailValidationToken(user=user).save()
+        token = EmailValidationToken(user=user)
+        token.save()
+        self.send_validation_email(user, token)
         return redirect('main:index')
 
 
@@ -39,4 +56,4 @@ class ValidateTokenEmailView(TemplateView):
             token.consume()
             return redirect('main:index')
         except Exception as e:
-            raise e
+            return HttpResponseBadRequest('Something went wrong with your token, please try again')
