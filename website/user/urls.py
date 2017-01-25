063b77991740c84d7e3c59e87ae1df4a061039d3
Dorian Amouroux <dor.amouroux@gmail.com>
2017-01-25 10:54:35 -0800

    legacy code ?

--- website/user/urls.py
+++ website/user/urls.py
@@ -4,6 +4,7 @@ from django.contrib.auth.decorators import login_required
 from . import views
 
 urlpatterns = [
+    # before authentication
     url(r'^register/?$',
         views.RegisterView.as_view(),
         name='register'),
@@ -16,12 +17,21 @@ urlpatterns = [
     url(r'^logout/?$',
         django_auth_views.logout,
         name='logout'),
+    # profile
     url(r'^profile/?$',
         login_required(views.ProfileView.as_view()),
         name='profile'),
     url(r'^profile/edit/?$',
         login_required(views.ProfileEditView.as_view()),
         name='edit-profile'),
+    # change password
+    url(r'^password-reset/?$',
+        django_auth_views.password_reset,
+        name='reset-password'),
+    url(r'^password-reset/done?$',
+        django_auth_views.password_reset_done,
+        name='reset-password-done'),
+    # token validation
     url(r'^resend_validation_email/?$',
         login_required(views.ResendValidationEmail.as_view()),
         name='resend-validation-email'),
