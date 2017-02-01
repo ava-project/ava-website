5a3f76b74efb439cbe6c66c19dfb40eba015b195
Dorian Amouroux <dor.amouroux@gmail.com>
2017-02-01 11:27:34 -0800

    test with travis

--- website/user/decorators.py
+++ website/user/decorators.py
@@ -1,9 +1,15 @@
 from django.http import HttpResponse
 
 def remote_login_required(view_func):
-
+    """
+    Decorator for views that checks that the user is logged in, redirecting
+    to the log-in page if necessary.
+    """
     def _wrapped_view(request, *args, **kwargs):
-        print(request.user)
+        """
+        Decorator for views that checks that the user is logged in, redirecting
+        to the log-in page if necessary.
+        """
         if request.user.is_authenticated:
             return view_func(request, *args, **kwargs)
         else:
