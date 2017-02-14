b85bef337850af2d74eed2043bf83ecbac8d9806
Dorian Amouroux <dor.amouroux@gmail.com>
2017-02-14 13:05:07 -0800

    bit of refactoring

--- website/main/urls.py
+++ website/main/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
-from . import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^$',
-        views.IndexView.as_view(),
+        TemplateView.as_view(template_name = "main/index.html"),
         name='index'
     ),
 ]
--- website/main/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
-
-class IndexView(TemplateView):
-    template_name = "main/index.html"
