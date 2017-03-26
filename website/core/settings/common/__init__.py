f9c0fe788fe2b364ae6a76c998d37ee9f5746387
Dorian Amouroux <dor.amouroux@gmail.com>
2017-03-26 12:06:43 -0700

    change collect static destination

--- website/core/settings/common/__init__.py
+++ website/core/settings/common/__init__.py
@@ -7,7 +7,7 @@ BASE_DIR = '/app'
 DEBUG = True
 SECRET_KEY = env.get('SECRET_KEY', 'weird punchy keyboard')
 STATIC_URL = '/static/'
-STATIC_ROOT = '/app' + STATIC_URL
+STATIC_ROOT = '/app/public'
 ROOT_URLCONF = 'core.urls'
 WSGI_APPLICATION = 'core.wsgi.application'
 
