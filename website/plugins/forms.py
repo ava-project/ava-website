57aa6c3f6ff5fc8883f08a2ddb6c6dcfa0d31ecc
Dorian Amouroux <dor.amouroux@gmail.com>
2017-04-18 14:24:05 -0700

    validator for zipfile

--- website/plugins/forms.py
+++ website/plugins/forms.py
@@ -1,5 +1,12 @@
 from django import forms
 
+from .validators import ZipArchiveValidator
+
 
 class UploadPluginForm(forms.Form):
-    archive = forms.FileField(label='Plugin .zip')
+    archive = forms.FileField(
+        label='Plugin .zip',
+        validators=[ZipArchiveValidator()]
+    )
+
+    # def clean(self):
