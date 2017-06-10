5d3e0ca4e1f5b5a8223499cca4d8fcd4991522e0
Dorian Amouroux <dor.amouroux@gmail.com>
2017-06-09 20:51:57 -0700

    add comment section for plugins

--- website/core/settings/common/apps.py
+++ website/core/settings/common/apps.py
@@ -4,9 +4,11 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
+    'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mail_templated',
+    'django_comments',
 
     'main',
     'plugins',
